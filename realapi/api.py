7b342c7da8fd40265a9bda5a65c8a0741f92e2e7
Francis Kiryowa <franciskiryowa68@gmail.com>
2018-09-12 09:43:46 -0400

    Add tests for api

--- realapi/api.py
+++ realapi/api.py
@@ -1,4 +1,7 @@
-"""This module defines api views"""
+"""
+This module defines api views
+
+"""
 from flask import jsonify
 from flask import request
 from flask.views import MethodView
@@ -7,9 +10,18 @@ from orders import Orders
 
 class ManageOrders(MethodView):
     """Class to define all the api end points"""
-    order1 = Orders(1,[{"item_id":1,"item_name":"pizza", "price":40000, "quantity":1}], "none", 23)
-    order2 = Orders(2,[{"item_id":1,"item_name":"fresh juice", "price":20000, "quantity":2}], "none", 45)
-    order3 = Orders(3,[{"item_id":1,"item_name":"fried fish", "price":30000, "quantity":3}], "none", 12)
+    order1 = Orders(
+        1, [{"item_id":1, "item_name":"pizza", "price":40000, "quantity":1}],
+        "none", 23
+    )
+    order2 = Orders(
+        2, [{"item_id":1, "item_name":"fresh juice", "price":20000, "quantity":2}],
+        "none", 45
+    )
+    order3 = Orders(
+        3, [{"item_id":1, "item_name":"fried fish", "price":30000, "quantity":3}],
+        "none", 12
+    )
 
     orders = [order1, order2, order3]
 
@@ -18,25 +30,57 @@ class ManageOrders(MethodView):
         if order_id is None:
             # return a list of orders
             return jsonify({'all orders':[order.__dict__ for order in self.orders]})
-        specific_order = [
-            order.__dict__ for order in self.orders 
-            if order.__dict__["order_id"] == order_id
-        ]
-        return jsonify({'order':specific_order[0]})
+        if isinstance(order_id, int):
+            if not order_id < 0:
+                if not isinstance(order_id, bool):
+                    specific_order = [
+                        order.__dict__ for order in self.orders
+                        if order.__dict__["order_id"] == order_id
+                    ]
+                    if not specific_order:
+                        #return jsonify({'Message':'No Order Found with Specified Route Parameter'})
+                        raise ValueError('No Order Found with Specified Route Parameter')
+                    return jsonify({'order':specific_order[0]})
+                else:
+                    raise TypeError('The route parameter cannot be a boolean')
+            else:
+                raise ValueError('The route parameter can not be a number less than zero')
+        else:
+            raise TypeError('The route parameter cannot be a String')
+
     def post(self):
         """funtion to place a new order"""
         # create a new user
         order = Orders(
-            len(self.orders) + 1, request.json['order_items'], 
+            len(self.orders) + 1, request.json['order_items'],
             None, request.json['user_id']
         )
         self.orders.append(order)
-        return jsonify(order.__dict__)       
+        return jsonify(order.__dict__)
     def put(self, order_id):
         """function to update a specific  order"""
         # update a single user
-        for order in self.orders:
-            if order.__dict__["order_id"] == order_id:
-                order_json = request.get_json()
-                order.__dict__['order_status'] = order_json['order_status']
-        return jsonify({'orders':[order.__dict__ for order in self.orders]})
+        if isinstance(order_id, int):
+            if not order_id < 0:
+                if not isinstance(order_id, bool):
+                    for order in self.orders:
+                        if order.__dict__["order_id"] == order_id:
+                            order_json = request.get_json()
+                            order.__dict__['order_status'] = order_json['order_status']
+                            return jsonify({'orders':[order.__dict__ for order in self.orders]})
+                        return jsonify({'Message':'No Order Found with Specified Route Parameter'})
+                else:
+                    raise TypeError(
+                        'The route parameter to update a specific\
+                         order status cannot be a boolean'
+                    )
+            else:
+                raise ValueError(
+                    'The route parameter to update a specific\
+                    order status cannot be an interger less than a zero'
+                )
+        else:
+            raise TypeError(
+                'The route parameter to update a specific \
+                order status cannot be a String'
+            )
